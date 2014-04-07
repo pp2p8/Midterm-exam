f0e1cddd1b731c3d31710ef9b5c7a29ea5e94d57
pp2p8 <pp2p8@mail.umkc.edu>
2014-04-06 23:40:22 -0500

    midterm exam report and source code

---
+++ Trip Router/C#/TripRouterLibrary/CheckIn.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FoursquareNET.Checkins
+{
+    public class CheckIn
+    {
+        private const string URL = "http://api.foursquare.com/v1/checkin";
+        private const Common.HttpRequestMethod HTTP_REQUEST_METHOD = Common.HttpRequestMethod.POST;
+
+        private List<Parameter> Parameters = new List<Parameter>();
+
+        /// <summary>
+        /// Allows you to check-in to a place.
+        /// </summary>
+        /// <param name="returnFormat"></param>
+        public CheckIn()
+        {
+
+        }
+
+        public Schema.Checkin Execute(Credential credential)
+        {
+            if (string.IsNullOrEmpty(_VenueId.Value) && string.IsNullOrEmpty(_Shout.Value) && string.IsNullOrEmpty(_VenueName.Value))
+                throw new Exception.OptionalParameterException("You must specify a VenueId, VenueName, or Shout");
+
+            Parameters.Clear();
+            Parameters.Add(_VenueId);
+            Parameters.Add(_VenueName);
+            Parameters.Add(_Shout);
+            Parameters.Add(_Private);
+            Parameters.Add(_Twitter);
+            Parameters.Add(_Facebook);
+            Parameters.Add(_Geolat);
+            Parameters.Add(_Geolong);
+
+            string result = Common.HTTPPost(URL, Parameters, credential, HTTP_REQUEST_METHOD);
+
+            return new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<FoursquareNET.Schema.CheckinObj>(result).Checkin;
+        }
+
+        #region Parameters
+
+        private Parameter _VenueId = new Parameter("VenueId", "vid", false);
+        private Parameter _VenueName = new Parameter("VenueName", "venue", false);
+        private Parameter _Shout = new Parameter("Shout", "shout", false);
+        private Parameter _Private = new Parameter("Private", "private", false);
+        private Parameter _Twitter = new Parameter("Twitter", "twitter", false);
+        private Parameter _Facebook = new Parameter("Facebook", "facebook", false);
+        private Parameter _Geolat = new Parameter("Geolat", "geolat", false);
+        private Parameter _Geolong = new Parameter("Geolong", "geolong", false);
+
+        /// <summary>
+        /// (optional, not necessary if you are 'shouting' or have a venue name). ID of the venue where you want to check-in
+        /// </summary>
+        public int? VenueId { set { _VenueId.Value = value != null ? value.ToString() : string.Empty; } }
+
+        /// <summary>
+        /// (optional, not necessary if you are 'shouting' or have a vid) if you don't have a venue ID or would rather prefer a 'venueless' checkin, pass the venue name as a string using this parameter. it will become an 'orphan' (no address or venueid but with geolat, geolong)
+        /// </summary>
+        public string Venue { set { _VenueName.Value = value; } }
+
+        /// <summary>
+        /// (optional) a message about your check-in. the maximum length of this field is 140 characters
+        /// </summary>
+        public string Shout { set { _Shout.Value = value; } }
+
+        /// <summary>
+        /// (optional). "true" means "don't show your friends". "false" means "show everyone"
+        /// </summary>
+        public bool? Private { set { _Private.Value = value != null ? (value.Value ? "true" : "false") : string.Empty; } }
+
+        /// <summary>
+        /// (optional, defaults to the user's setting). "true" means "send to Twitter". "false" means "don't send to Twitter"
+        /// </summary>
+        public bool? Twitter { set { _Twitter.Value = value != null ? (value.Value ? "true" : "false") : string.Empty; } }
+
+        /// <summary>
+        /// (optional, defaults to the user's setting). "true" means "send to Facebook". "false" means "don't send to Facebook"
+        /// </summary>
+        public bool? Facebook { set { _Facebook.Value = value != null ? (value.Value ? "true" : "false") : string.Empty; } }
+
+        /// <summary>
+        /// (optional, but recommended)
+        /// </summary>
+        public string Geolat { set { _Geolat.Value = value; } }
+
+        /// <summary>
+        /// (optional, but recommended)
+        /// </summary>
+        public string Geolong { set { _Geolong.Value = value; } }
+
+        #endregion
+    }
+}
---
+++ Trip Router/C#/TripRouterLibrary/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace FoursquareNET.User
+{
+    public class GetDetail
+    {
+        private const string URL = "http://api.foursquare.com/v1/user";
+        private const Common.HttpRequestMethod HTTP_REQUEST_METHOD = Common.HttpRequestMethod.GET;
+        private List<Parameter> Parameters = new List<Parameter>();
+
+        /// <summary>
+        /// Returns profile information (badges, etc) for a given user. If the user has recent check-in data (ie, if the user is self or is a friend of the authenticating user), this data will be returned as well in a checkin block.
+        /// </summary>
+        /// <param name="returnFormat"></param>
+        public GetDetail()
+        {
+        }
+
+        public Schema.User Execute(Credential credential)
+        {
+            Parameters.Clear();
+            Parameters.Add(_UserId);
+            Parameters.Add(_ShowBadges);
+            Parameters.Add(_ShowMayorships);
+
+            string result = Common.HTTPPost(URL, Parameters, credential, HTTP_REQUEST_METHOD);
+
+            return new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<FoursquareNET.Schema.UserObj>(result).User;
+        }
+
+        #region Parameters
+
+        private Parameter _UserId = new Parameter("UserId", "uid", false);
+        private Parameter _ShowBadges = new Parameter("ShowBadges", "badges", false);
+        private Parameter _ShowMayorships = new Parameter("ShowMayorships", "mayor", false);
+
+        /// <summary>
+        /// userid for the user whose information you want to retrieve. if you do not specify a 'uid', the authenticated user's profile data will be returned.
+        /// </summary>
+        public int? UserId { set { _UserId.Value = value != null ? value.ToString() : string.Empty; } }
+
+        /// <summary>
+        /// (optional, default: false) set to "true" to also show badges for this user. by default, this will show badges earned worldwide.
+        /// </summary>
+        public bool? ShowBadges { set { _ShowBadges.Value = value != null ? (value.Value ? "1" : "0") : string.Empty; } }
+
+        /// <summary>
+        /// (optional, default: false) set to "true" to also show venues for which this user is a mayor. by default, this will show mayorships worldwide.
+        /// </summary>
+        public bool? ShowMayorships { set { _ShowMayorships.Value = value != null ? (value.Value ? "1" : "0") : string.Empty; } }
+
+        #endregion
+    }
+}
+
